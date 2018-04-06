154739826ca68c757fd2b5b990a6b14e5cf2b10d
David Ko <david.ko@pvtmethod.com>
2018-04-06 17:28:23 -0400

    Edit tf

--- tunl.tf
+++ tunl.tf
@@ -5,10 +5,9 @@ provider "aws" {
 
 resource "aws_instance" "intermediary" do {
   instance_type = "t2.small"
-  ami = "ami-43a15f3e" # Ubuntu 16.04
+  ami = "ami-43a15f3e" # Amazon|Ubuntu 16.04
   associate_public_ip_address = true
-  subnet_id = "" # [TODO] this must launch into a public subnet in the same VPC
-                 # as the private subnet we want to get to
+  subnet_id = ""
 
   tags {
     Name = "Tunl Intermediate {{timestamp}}"
